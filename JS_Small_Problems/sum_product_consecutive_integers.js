62d79c0059648fa3339fda664a63cc9287aa128c
Cody Meireis <codymeireis@gmail.com>
2020-06-30 22:20:49 -0500

    adding my solution to sum or product consecutive integers

--- JS_Small_Problems/sum_product_consecutive_integers.js
+++ JS_Small_Problems/sum_product_consecutive_integers.js
@@ -21,6 +21,41 @@ Algorithm:
   1.  Create readlinesync to ask user for input.
   2.  Create enterNumber variable to store value from user input: 'Please enter an integer greater than 0:'
   3.  Create sumOrProduct variable to store value from user input:  'Enter "s" to calculate the sum, or "p" to calculate the product:'
-  4.  Take the inputs into a function calcSumOrProduct(enterNumber, sumOrProduct)
-  5.  
+  4.  Take the inputs into functions calcSum(enterNumber)  and calcProduct(enterNumber)
+  5.  Iterate through numbers, incrementing by 1, starting at 1 until number is <= to user input.
+  6.  If sumOrProduct = s.  Let total = total += i.
+  7.  Output to user is `The sum of the integers between 1 and ${enterNumber} is ${total}`
+  7.  If sumOrProduct = p.  Let total = total *= i.
+  8.  Output to user is `The product of the integers between 1 and ${enterNumber} is ${total}`
+
 Code */
+
+const readLineSync = require('readline-sync');
+
+let enterNumber = readLineSync.question('Please enter an integer greater than 0:');
+let sumOrProduct = readLineSync.question('Enter "s" to calculate the sum, or "p" to calculate the product:');
+
+function calcSum(enterNumber) {
+  let total = 0;
+  for (let i = 1; i <= enterNumber; i += 1) {
+    total += i;
+  }
+  return total;
+}
+
+function calcProduct(enterNumber) {
+  let total = 1;
+  for (let i = 1; i <= enterNumber; i += 1) {
+    total *= i;
+  }
+  return total;
+}
+if (sumOrProduct === "s") {
+        let sum = calcSum(enterNumber);
+        console.log(`The sum of the integers between 1 and ${enterNumber} is ${sum}`);
+    } else if (sumOrProduct === "p") {
+      let product = calcProduct(enterNumber);
+      console.log(`The product of the integers between 1 and ${enterNumber} is ${product}`);
+    } else {
+        console.log('You did not enter "s" or "p".');
+    }
